37374d54f30905ee6e710d6b9d2b219740ab621e
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-07-18 11:58:39 -0500

    testing

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,131 +1,112 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-
-
-  router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'post_content'
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username', 'twitter', 'github']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username', 'twitter', 'github']
-        }
-      ]
-    })
-      .then(dbDataPost => {
-        if (!dbDataPost) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-  
-        // serialize the data
-        const post = dbDataPost.get({ plain: true });
-
-        res.render('edit-post', {
-            post,
-            loggedIn: true
-            });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-});
-
-router.get('/', withAuth, (req, res) => {
-  Post.findAll({
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
     where: {
-      // use the ID from the session
-      user_id: req.session.user_id
+      id: req.params.id,
     },
-    attributes: [
-      'id',
-      'title',
-      'created_at',
-      'post_content'
-    ],
+    attributes: ["id", "title", "created_at", "post_content"],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ['username', 'twitter', 'github']
-        }
+          attributes: ["username", "twitter", "github"],
+        },
       },
       {
         model: User,
-        attributes: ['username', 'twitter', 'github']
-      }
-    ]
+        attributes: ["username", "twitter", "github"],
+      },
+    ],
   })
-    .then(dbDataPost => {
-      // serialize data before passing to template
-      const posts = dbDataPost.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
+    .then((dbDataPost) => {
+      if (!dbDataPost) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const post = dbDataPost.get({ plain: true });
+
+      res.render("edit-post", {
+        post,
+        loggedIn: true,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get('/create/', withAuth, (req, res) => {
-    Post.findAll({
-      where: {
-        // use the ID from the session
-        user_id: req.session.user_id
-      },
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'post_content'
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username', 'twitter', 'github']
-          }
-        },
-        {
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
-          attributes: ['username', 'twitter', 'github']
-        }
-      ]
+          attributes: ["username", "twitter", "github"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "twitter", "github"],
+      },
+    ],
+  })
+    .then((dbDataPost) => {
+      // serialize data before passing to template
+      const posts = dbDataPost.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
     })
-      .then(dbDataPost => {
-        // serialize data before passing to template
-        const posts = dbDataPost.map(post => post.get({ plain: true }));
-        res.render('create-post', { posts, loggedIn: true });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+router.get("/create/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "twitter", "github"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "twitter", "github"],
+      },
+    ],
+  })
+    .then((dbDataPost) => {
+      // serialize data before passing to template
+      const posts = dbDataPost.map((post) => post.get({ plain: true }));
+      res.render("create-post", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
